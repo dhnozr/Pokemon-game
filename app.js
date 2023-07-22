72271eb5f4cbaf15eea58daee8898b0a083725c4
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-07-23 00:10:06 +0300

    flipCard function created

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // take all the game card's
 const cards = document.querySelectorAll('.card');
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add('flip');
+
+  if (!hasFlipped) {
+    // first click
+    firstCard = this;
+    hasFlipped = true;
+    return;
+  } else {
+    // second click
+    secondCard = this;
+    hasFlipped = false;
+
+    checkForMatch();
+  }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
